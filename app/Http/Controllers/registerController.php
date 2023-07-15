b51b445da28a3e0708fa925794f7a031f79f792a
rokhmad92 <rokhmad92@gmail.com>
2023-07-15 09:54:24 +0700

    berhasil memakai sms gateway dan membuat json

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -41,6 +41,45 @@ class registerController extends Controller
         $user = $this->userRepository->storeUser($request);
         $this->userRepository->sendOtp($user);
 
+        // sms infobip
+            $api_Key = '9ecaf3082130b29eb2153dae4a55e43b-a407507f-76f2-462f-9238-5b6cd761aa3b';
+            $base_url = 'lz51nr.api.infobip.com';
+            $json = [
+                'messages' => [
+                    'destinations' => [
+                        'to' => '62895336397742'
+                    ],
+                    'from' => 'Rokhmad',
+                    'text' => 'berhasil convert array ke json'
+                ]
+            ];
+            $result = json_encode($json);
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://$base_url/sms/2/text/advanced",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $result,
+                CURLOPT_HTTPHEADER => array(
+                    "Authorization: App $api_Key",
+                    'Content-Type: application/json',
+                    'Accept: application/json'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
+        // end sms
+
         return to_route('verifyOTP', ['nomer' => $request->nomer]);
     }
 
